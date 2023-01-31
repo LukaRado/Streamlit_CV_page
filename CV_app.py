fb035af92aa6083aa0579b829faf54f54162cbb7
LukaRado <luka.radosavljevic@hotmail.comm>
2023-01-31 20:20:25 +0100

    Justeringer

--- CV_app.py
+++ CV_app.py
@@ -12,18 +12,16 @@ profile_pic = current_dir / "assets" / "profile-pic.png"
 
 
 # --- GENERAL SETTINGS ---
-PAGE_TITLE = "Digital CV | John Doe"
+PAGE_TITLE = "Digital CV | Luka Radosavljevic"
 PAGE_ICON = ":wave:"
-NAME = "John Doe"
+NAME = "Luka Radosavljevic"
 DESCRIPTION = """
-Senior Data Analyst, assisting enterprises by supporting data-driven decision-making.
+Stud. Cand. Merc. MAC. Med fokus på automatisering og optimering af processer, ved hjælp af Data Science.
 """
-EMAIL = "johndoe@email.com"
+EMAIL = "luka.radosavljevic@hotmail.com"
 SOCIAL_MEDIA = {
-    "YouTube": "https://youtube.com/c/codingisfun",
-    "LinkedIn": "https://linkedin.com",
-    "GitHub": "https://github.com",
-    "Twitter": "https://twitter.com",
+    "LinkedIn": "https://www.linkedin.com/in/luka-radosavljevic-9466bb168/",
+    "GitHub": "https://github.com/LukaRado",
 }
 PROJECTS = {
     "🏆 Sales Dashboard - Comparing sales across three stores": "https://youtu.be/Sb0A9i6d320",
