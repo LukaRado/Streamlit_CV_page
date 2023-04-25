dbebaea4312ad690beb79b39ea30b560c19536da
LukaRado <112628465+LukaRado@users.noreply.github.com>
2023-04-25 17:44:44 +0200

    Added new CV file

--- CV_app.py
+++ CV_app.py
@@ -7,7 +7,7 @@ from PIL import Image
 # --- PATH SETTINGS ---
 current_dir = Path(__file__).parent if "__file__" in locals() else Path.cwd()
 css_file = current_dir / "styles" / "main.css"
-resume_file = current_dir / "assets" / "CV_Luka_Radosavljevic (8).pdf"
+resume_file = current_dir / "assets" / "CV - Luka Radosavljevic.pdf"
 profile_pic = current_dir / "assets" / "profile-pic_Luka.png"
 
 
